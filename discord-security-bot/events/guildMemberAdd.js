e9d13b89baf3c4de06f45ce8834f8993b4de7df7
zkhan63321-design <zkhan63321@gmail.com>
2026-05-13 14:13:37 +0530

    Delete discord-security-bot directory

--- discord-security-bot/events/guildMemberAdd.js
+++
@@ -1,8 +0,0 @@
-// events/guildMemberAdd.js
-module.exports = {
-  name: 'guildMemberAdd',
-  async execute(member, client) {
-    // Log new member joins (for audit purposes)
-    console.log(`[JOIN] ${member.user.tag} joined ${member.guild.name}`);
-  }
-};
